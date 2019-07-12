6c368495c95e010540c9acca15e4054c84f40899
Andrew Rosário <andrew.arosario@gmail.com>
2019-07-12 20:48:16 -0300

    implementado scrobble pelo perfil do usuario

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -10,22 +10,34 @@ import { ArtistService } from 'src/app/services/lastfm/artist/artist.service';
 export class SearchComponent implements OnInit {
 
   // textSearch: string;
-  results;
+  results: PaginatedData<Searchable>;
+  loading: boolean;
+
+  pageEvent = {
+    pageIndex: 0,
+    pageSize: 10,
+    length: 0
+  };
+  previousPageSize: number;
 
   constructor(
     private route: ActivatedRoute,
-    private artistService: ArtistService) { }
+    private artistService: ArtistService
+  ) { }
 
   ngOnInit() {
-
     this.route.paramMap
       .subscribe(params => this.search(params.get('search')));
-
   }
 
   async search(textSearch: string) {
-    const results = await this.artistService.search(textSearch, 50, 1);
-    console.log(results);
+    const page = {...this.pageEvent};
+    const searchResults = await this.artistService.search(textSearch, 50, 1);
+    this.results[page.pageIndex] = searchResults.results;
+  }
+
+  get currentResults(): Searchable[] {
+    return this.results[this.pageEvent.pageIndex];
   }
 
 }
--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from 'src/app/services/lastfm/user/user.service';
+import { ScrobbleService } from 'src/app/services/lastfm/scrobble/scrobble.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -12,7 +13,8 @@ export class UserProfileComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private userService: UserService
+    private userService: UserService,
+    private scrobbleService: ScrobbleService
   ) { }
 
   user: User;
@@ -34,6 +36,19 @@ export class UserProfileComponent implements OnInit {
     console.log(this.recentTracks);
     console.log(this.recentTracks[0].artist['#text'] + this.recentTracks[0].name);
 
+  }
+
+  scrobbleTrack(track) {
+    const simpleTrack: SimpleTrack = {
+      artist: track.artist['#text'],
+      song: track.name,
+      album: track.album['#text'],
+    };
+
+    this.scrobbleService
+      .scrobble(simpleTrack)
+      .then(() => console.log('Success'))
+      .catch(() => console.log('Error'));
   }
 
 }
